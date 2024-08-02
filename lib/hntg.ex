b06b83f2289e20f7120b0c73cc5b1f335782d3e0
Chay Choong <chaychoong@gmail.com>
2024-08-02 23:35:06 +0800

    init: mix new
    Generate a new project using `mix new hntg --sup`
    
    Add req as dependency for making requests

---
+++ lib/hntg.ex
@@ -0,0 +1,18 @@
+defmodule Hntg do
+  @moduledoc """
+  Documentation for `Hntg`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Hntg.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/hntg_test.exs
@@ -0,0 +1,8 @@
+defmodule HntgTest do
+  use ExUnit.Case
+  doctest Hntg
+
+  test "greets the world" do
+    assert Hntg.hello() == :world
+  end
+end
